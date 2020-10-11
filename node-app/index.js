929f2400823110a011148bb66729ad3b1c9b9d5a
1Mill <erik.ekberg@outlook.com>
2020-10-10 19:50:12 -0700

    Formatting part 2

--- node-app/index.js
+++ node-app/index.js
@@ -7,6 +7,6 @@ AWS.config.update({
 	secretAccessKey: process.env.AWS_ACCESSKEY_SECRETKEY,
 })
 
-const lambda = new AWS.Lambda({ apiVersion: '2015-03-31' });
+const lambda = new AWS.Lambda({ apiVersion: '2015-03-31' })
 
 console.log('Hello world!')
