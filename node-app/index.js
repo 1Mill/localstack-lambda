2fce8a1bc0891e2deecadd818e4445ecc6173392
1Mill <erik.ekberg@outlook.com>
2020-10-11 12:51:25 -0700

    Update index.js

--- node-app/index.js
+++ node-app/index.js
@@ -1,9 +1,5 @@
 const AWS = require('aws-sdk')
 
-console.log(process.env.AWS_ACCESSKEY_ID)
-console.log(process.env.AWS_REGION)
-console.log(process.env.AWS_ACCESSKEY_SECRETKEY)
-console.log(process.env.AWS_ENDPOINT)
 const lambda = new AWS.Lambda({
 	accessKeyId: process.env.AWS_ACCESSKEY_ID,
 	apiVersion: '2015-03-31',
