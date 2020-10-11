b866864e17ff7b506d2fa97879e095dad9b5f736
1Mill <erik.ekberg@outlook.com>
2020-10-10 19:34:10 -0700

    Connect to localstack

--- node-app/index.js
+++ node-app/index.js
@@ -1,1 +1,10 @@
+const AWS = require('aws-sdk')
+
+AWS.config.update({
+	accessKeyId: process.env.AWS_ACCESSKEY_ID,
+	endpoint: process.env.AWS_ENDPOINT || undefined,
+	region: process.env.AWS_REGION,
+	secretAccessKey: process.env.AWS_ACCESSKEY_SECRETKEY,
+})
+
 console.log('Hello world!')
