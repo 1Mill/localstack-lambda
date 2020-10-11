ecdea86de2a3e0b317e01adcad31b3360a077624
1Mill <erik.ekberg@outlook.com>
2020-10-11 11:42:10 -0700

    Put AWS credentials in AWS.Lambda instead of AWS.config.update to reduce complexity / stesp

--- node-app/index.js
+++ node-app/index.js
@@ -1,13 +1,16 @@
 const AWS = require('aws-sdk')
 
-AWS.config.update({
+console.log(process.env.AWS_ACCESSKEY_ID)
+console.log(process.env.AWS_REGION)
+console.log(process.env.AWS_ACCESSKEY_SECRETKEY)
+console.log(process.env.AWS_ENDPOINT)
+const lambda = new AWS.Lambda({
 	accessKeyId: process.env.AWS_ACCESSKEY_ID,
+	apiVersion: '2015-03-31',
 	endpoint: process.env.AWS_ENDPOINT || undefined,
 	region: process.env.AWS_REGION,
 	secretAccessKey: process.env.AWS_ACCESSKEY_SECRETKEY,
 })
-
-const lambda = new AWS.Lambda({ apiVersion: '2015-03-31' })
 const params = {
 	FunctionName: 'node-lambda',
 	InvocationType: 'RequestResponse',
