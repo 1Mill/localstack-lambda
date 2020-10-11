5b2e98203f5e7a05a297901fb20061879414f109
1Mill <erik.ekberg@outlook.com>
2020-10-10 19:46:00 -0700

    Run script to auto-setup / configure lambda

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+AWS_ENDPOINT=http://localhost:4566/
+HOST_ABSOLUTEPATH=/$(cd -- ./node-lambda && pwd)
+LAMBDA_FUNCTIONNAME=node-lambda
+LAMBDA_HANDLER=index.handler
+LAMBDA_RUNTIME=nodejs12.x
+
+# * Delete function if it already exists
+aws --endpoint-url=$AWS_ENDPOINT \
+	lambda delete-function \
+		--function-name $LAMBDA_FUNCTIONNAME
+
+# * Create function
+aws --endpoint-url=$AWS_ENDPOINT \
+	lambda create-function \
+		--code S3Bucket="__local__",S3Key="${HOST_ABSOLUTEPATH}" \
+		--function-name ${LAMBDA_FUNCTIONNAME} \
+		--handler ${LAMBDA_HANDLER} \
+		--role just-has-to-exist \
+		--runtime ${LAMBDA_RUNTIME}
